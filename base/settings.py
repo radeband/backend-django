0edfd91b0a9bac4826d2d2319ce37a4d44adec89
Javad Naghiloo <dukejavad@gmail.com>
2022-12-17 20:55:12 +0330

    fix(requirements): fix psycopg2 package installation

--- base/settings.py
+++ base/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASS'),
